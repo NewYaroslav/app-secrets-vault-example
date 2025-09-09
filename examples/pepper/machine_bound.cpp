5d473032cff6245f7fb1ab95c6d3453a66713676
Aster Seker <git.electroyar@gmail.com>
2025-09-09 05:01:33 +0300

    chore: update machine_bound.cpp

--- examples/pepper/machine_bound.cpp
+++ examples/pepper/machine_bound.cpp
@@ -3,20 +3,18 @@
 #include <fstream>
 #include <cstdlib>
 
-namespace pepper {
-namespace machine_bound {
+namespace pepper::machine_bound {
 
-std::vector<uint8_t> get_machine_secret(const Config&) {
-    std::ifstream f("/etc/machine-id");
-    std::string id;
-    if (f) {
-        std::getline(f, id);
+    std::vector<uint8_t> get_machine_secret(const Config&) {
+        std::ifstream f("/etc/machine-id");
+        std::string id;
+        if (f) {
+            std::getline(f, id);
+        }
+        const char* user = std::getenv("USER");
+        if (user) id += user;
+        if (id.empty()) return {};
+        return hmac_hash::sha256(id.data(), id.size());
     }
-    const char* user = std::getenv("USER");
-    if (user) id += user;
-    if (id.empty()) return {};
-    return hmac_hash::sha256(id.data(), id.size());
-}
 
-} // namespace machine_bound
-} // namespace pepper
+} // namespace pepper::machine_bound
